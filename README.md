1a9d2a50a68f18823d0e145291432557151224cb
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2021-06-01 00:08:34 +0200

    My profile dev page

---
+++ README.md
@@ -0,0 +1,2 @@
+### My profile dev page
+Visit [https://cesarsan.dev](cesarsan.dev)
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    future: {},
+    purge: ['./index.html'],
+    theme: {
+        colors: {
+            white: '#FFFFFF',
+            body: '#E1E1F3',
+            purple: '#38178F',
+            gray: {
+                DEFAULT: '#2D3043',
+                dark: '#222433'
+            },
+            blue: {
+                DEFAULT: '#2B98E1',
+                dark: '#000624'
+            },
+            pink: '#D1316C',
+            green: '#519F20',
+            orange: {
+                light: '#E2975C',
+                DEFAULT: '#F95F59'
+            },
+            yellow: '#FBC12D',
+        },
+    },
+    variants: {},
+    plugins: [],
+}
